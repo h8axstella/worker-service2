6734d24cca6aa6758c73b6a4d56e6ec473ef92ad
h8axstella <s.drozdov@hoststella.com>
2024-05-23 10:32:33 +0300

    main func modify

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ func main() {
 	config.InitConfig()
 	database.Init()
 	go startWorkerProcessor()
-	go course.ScheduleBTCProcessing()
+	go scheduleBTCProcessing()
 	select {}
 }
 
 func startWorkerProcessor() {
-	ticker := time.NewTicker(24 * time.Hour)
+	ticker := time.NewTicker(12 * time.Hour)
 	defer ticker.Stop()
 	log.Printf("Starting initial worker processing at %s\n", time.Now())
 	worker.ProcessWorkers()
@@ -30,6 +30,22 @@ func startWorkerProcessor() {
 			log.Printf("Starting worker processing at %s\n", time.Now())
 			worker.ProcessWorkers()
 			log.Printf("Worker processing completed at %s\n", time.Now())
+		}
+	}
+}
+
+// Исправленная функция scheduleBTCProcessing
+func scheduleBTCProcessing() {
+	// First run immediately
+	course.ProcessBTCPrice()
+
+	ticker := time.NewTicker(12 * time.Hour)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			log.Println("Scheduled BTC price processing started.")
+			course.ProcessBTCPrice()
 		}
 	}
 }
